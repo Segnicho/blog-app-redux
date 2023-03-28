a028d2c81e4c9a0ebb09396bc48020a20c36f362
Segni Desta <segnidesta60@gmail.com>
2023-03-28 09:15:20 +0300

    test-add

---
+++ testing/arthimetic_operations.py
@@ -0,0 +1,20 @@
+class ArthimeticClass:
+    def add(x, y):
+        # adding two numbers
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
+        return x + y
+    def subtract(x, y):
+        # subtracting two numbers
+        if not isinstance(x, (int, float)) or not isinstance(y, (int, float)):
+            raise TypeError("Both inputs should be numbers")
+        return x - y
+    def multiply(x, y):
+        # multiplying two numbers
+        return x * y
+    def divide(x, y):
+        # dividing two numbers
+        return x / y
+
+
+
